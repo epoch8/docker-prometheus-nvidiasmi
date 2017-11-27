7d8f5c547ec4aaa50c29d022829e1b72d1c1a007
Andrey Tatarinov <elephantropy@gmail.com>
2017-11-28 02:39:52 +0300

    fix for error values

--- src/app.go
+++ src/app.go
@@ -73,6 +73,16 @@ func filterNumber(value string) string {
     return r.ReplaceAllString(value, "")
 }
 
+func writeMetric(w http.ResponseWriter, name string, meta string, value string) (n int, err error) {
+  filtered_value := filterNumber(value)
+
+  if filtered_value != "" {
+    return io.WriteString(w, formatValue(name, meta, filtered_value))
+  } else {
+    return 0, nil
+  }
+}
+
 func metrics(w http.ResponseWriter, r *http.Request) {
     log.Print("Serving /metrics")
 
@@ -104,25 +114,25 @@ func metrics(w http.ResponseWriter, r *http.Request) {
     for _, GPU := range xmlData.GPUs {
         meta := "name=\"" + GPU.ProductName + " [" + GPU.MinorNumber + "]\"" + ", " + "uuid=\"" + GPU.UUID + "\""
 
-        io.WriteString(w, formatValue("nvidiasmi_fan_speed", meta, filterNumber(GPU.FanSpeed)))
-        io.WriteString(w, formatValue("nvidiasmi_memory_usage_total", meta, filterNumber(GPU.FbMemoryUsage.Total)))
-        io.WriteString(w, formatValue("nvidiasmi_memory_usage_used", meta, filterNumber(GPU.FbMemoryUsage.Used)))
-        io.WriteString(w, formatValue("nvidiasmi_memory_usage_free", meta, filterNumber(GPU.FbMemoryUsage.Free)))
-        io.WriteString(w, formatValue("nvidiasmi_utilization_gpu", meta, filterNumber(GPU.Utilization.GPUUtil)))
-        io.WriteString(w, formatValue("nvidiasmi_utilization_memory", meta, filterNumber(GPU.Utilization.MemoryUtil)))
-        io.WriteString(w, formatValue("nvidiasmi_temp_gpu", meta, filterNumber(GPU.Temperature.GPUTemp)))
-        io.WriteString(w, formatValue("nvidiasmi_temp_gpu_max", meta, filterNumber(GPU.Temperature.GPUTempMaxThreshold)))
-        io.WriteString(w, formatValue("nvidiasmi_temp_gpu_slow", meta, filterNumber(GPU.Temperature.GPUTempSlowThreshold)))
-        io.WriteString(w, formatValue("nvidiasmi_power_draw", meta, filterNumber(GPU.PowerReadings.PowerDraw)))
-        io.WriteString(w, formatValue("nvidiasmi_power_limit", meta, filterNumber(GPU.PowerReadings.PowerLimit)))
-        io.WriteString(w, formatValue("nvidiasmi_clock_graphics", meta, filterNumber(GPU.Clocks.GraphicsClock)))
-        io.WriteString(w, formatValue("nvidiasmi_clock_graphics_max", meta, filterNumber(GPU.MaxClocks.GraphicsClock)))
-        io.WriteString(w, formatValue("nvidiasmi_clock_sm", meta, filterNumber(GPU.Clocks.SmClock)))
-        io.WriteString(w, formatValue("nvidiasmi_clock_sm_max", meta, filterNumber(GPU.MaxClocks.SmClock)))
-        io.WriteString(w, formatValue("nvidiasmi_clock_mem", meta, filterNumber(GPU.Clocks.MemClock)))
-        io.WriteString(w, formatValue("nvidiasmi_clock_mem_max", meta, filterNumber(GPU.MaxClocks.MemClock)))
-        io.WriteString(w, formatValue("nvidiasmi_clock_video", meta, filterNumber(GPU.Clocks.VideoClock)))
-        io.WriteString(w, formatValue("nvidiasmi_clock_video_max", meta, filterNumber(GPU.MaxClocks.VideoClock)))
+        writeMetric(w, "nvidiasmi_fan_speed", meta, GPU.FanSpeed)
+        writeMetric(w, "nvidiasmi_memory_usage_total", meta, GPU.FbMemoryUsage.Total)
+        writeMetric(w, "nvidiasmi_memory_usage_used", meta, GPU.FbMemoryUsage.Used)
+        writeMetric(w, "nvidiasmi_memory_usage_free", meta, GPU.FbMemoryUsage.Free)
+        writeMetric(w, "nvidiasmi_utilization_gpu", meta, GPU.Utilization.GPUUtil)
+        writeMetric(w, "nvidiasmi_utilization_memory", meta, GPU.Utilization.MemoryUtil)
+        writeMetric(w, "nvidiasmi_temp_gpu", meta, GPU.Temperature.GPUTemp)
+        writeMetric(w, "nvidiasmi_temp_gpu_max", meta, GPU.Temperature.GPUTempMaxThreshold)
+        writeMetric(w, "nvidiasmi_temp_gpu_slow", meta, GPU.Temperature.GPUTempSlowThreshold)
+        writeMetric(w, "nvidiasmi_power_draw", meta, GPU.PowerReadings.PowerDraw)
+        writeMetric(w, "nvidiasmi_power_limit", meta, GPU.PowerReadings.PowerLimit)
+        writeMetric(w, "nvidiasmi_clock_graphics", meta, GPU.Clocks.GraphicsClock)
+        writeMetric(w, "nvidiasmi_clock_graphics_max", meta, GPU.MaxClocks.GraphicsClock)
+        writeMetric(w, "nvidiasmi_clock_sm", meta, GPU.Clocks.SmClock)
+        writeMetric(w, "nvidiasmi_clock_sm_max", meta, GPU.MaxClocks.SmClock)
+        writeMetric(w, "nvidiasmi_clock_mem", meta, GPU.Clocks.MemClock)
+        writeMetric(w, "nvidiasmi_clock_mem_max", meta, GPU.MaxClocks.MemClock)
+        writeMetric(w, "nvidiasmi_clock_video", meta, GPU.Clocks.VideoClock)
+        writeMetric(w, "nvidiasmi_clock_video_max", meta, GPU.MaxClocks.VideoClock)
     }
 }
 
